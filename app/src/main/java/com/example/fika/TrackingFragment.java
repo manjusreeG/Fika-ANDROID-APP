c4c4d3506bbb5a9c423d7c79632b8a525f7ec7a7
manjusreeG <manjugopi27@gmail.com>
2020-01-23 22:00:51 +0100

    Adding Payment and Tracking Fragment
    -navigating order page to payment and payment to tracking

---
+++ app/src/main/java/com/example/fika/TrackingFragment.java
@@ -0,0 +1,27 @@
+package com.example.fika;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TrackingFragment extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_tracking, container, false);
+
+        return view;
+    }
+
+
+}
